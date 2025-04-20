6f8f508cd502f362bed6c861fda00b052b7ab410
LilliAP <lillipmhsap@gmail.com>
2025-04-19 22:20:22 -0500

    Created main app theme

---
+++ lib/widgets/app_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppTheme{
+  static final ColorScheme appColorScheme = ColorScheme.fromSeed(
+    seedColor: Color(0xff825A3C),
+    primary: Color(0xff84634a),
+    secondary: Color(0xff825A3C),
+    surface: Color(0xffF7EBDF),
+    onPrimary: Color(0xff372414),
+    onSecondary: Color(0xff372414),
+    onSurface: Color(0xff372414)
+  );
+  static final ThemeData appThemeData = ThemeData(
+    useMaterial3: true,
+    colorScheme: appColorScheme,
+  );
+}
