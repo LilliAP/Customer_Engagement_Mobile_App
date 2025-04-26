9d6fd308621e15719cab05fa82f53a6dfad13864
LilliAP <lillipmhsap@gmail.com>
2025-04-25 21:32:23 -0500

    Added connection to firestore database

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:se330_project_2/widgets/app_bottom_navbar.dart';
 import 'package:se330_project_2/widgets/app_theme.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  await FirebaseMessaging.instance.requestPermission(); // gets perms for notifications
+
   runApp(const MyApp());
 }
 
